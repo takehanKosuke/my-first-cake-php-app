16726b2f103cdd1b47ea7608f860c25fd8bc490d
takehanKosuke <kkoussuke530@icloud.com>
2018-12-28 10:20:28 +0900

    restrict for lessons

--- cms/src/Controller/LessonsController.php
+++ cms/src/Controller/LessonsController.php
@@ -6,7 +6,10 @@
   {
     public function index()
     {
-      $lessons = $this->Lessons->find('all');
+      $lessons = $this->Lessons->find('all')
+                  ->order(['title' => 'DESC'])
+                  ->limit(2)
+                  ->where(['title like' => '%理学']);
       $this->set(compact('lessons'));
     }
   }
